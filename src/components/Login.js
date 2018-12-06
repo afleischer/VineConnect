6ef344ba81661fb2db03e8ae0afb2c974e85fb3a
afleischer <afleischer1337@gmail.com>
2018-12-06 07:35:48 -0800

    built out login section

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,11 @@ class Login extends React.Component{
 
 	}
 
+	state = {
+		value_uid: '',
+		value_pass: ''
+	}
+
 
 	/**
 	* On login, verify if the user is present server-side
@@ -14,19 +19,40 @@ class Login extends React.Component{
 
 	validateUser(){
 		
+		//compare the entered state
+
+		//encrypt the password and validate it on the server 
 	}
 
+	updateUID(event){
+	    this.setState({value_id: event.target.value});
+	}
 
+	updatePass(event){
+		this.setState({value_id: event.target.value});
 
-
+		/***
+		 * Need to set some type of timeout so that the client's password state
+		 * is reset after a period of time- likely as long as the user is logged in
+		 */
+	}
 
 
 	render(){
 
 		return(
 
+			<form onSubmit={this.validateUser}>
+				<label for = "uname">username:</label>
+					<input onChange = {this.updateUID} name="uname"></input>
+				<label for = "pass">password:</label>
+					<input onChange = {this.updatePass} namse = "pass" type = "password"></input>
+				<input type="submit">SUBMIT</input>
+			</form>
 
-		)
+		);
 	}
 
 }
+
+export default Login;
