99be175dafe22f27f06fae9b9fb31618b9bae0af
afleischer <afleischer1337@gmail.com>
2018-12-05 19:06:15 -0800

    adding login.js component

---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+class Login extends React.Component{
+	constructor(props){
+		super(props);
+
+	}
+
+
+	/**
+	* On login, verify if the user is present server-side
+	**/
+
+	validateUser(){
+		
+	}
+
+
+
+
+
+
+	render(){
+
+		return(
+
+
+		)
+	}
+
+}
