076402c1e22c947f41981eb067f693fbc24121cc
afleischer <afleischer1337@gmail.com>
2019-01-12 10:10:23 -0800

    more contactUPdate stuff

--- src/ContactModal.js
+++ src/ContactModal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+//TODO: Build this out
 
 class ContactModal extends React.Component{
 	constructor(props){
@@ -8,7 +8,13 @@ class ContactModal extends React.Component{
 	}
 
 	state={
-		clicked : false
+		opened : false
+	}
+
+	closeModal = () => {
+		this.setState({
+			opened: false
+		})
 	}
 
 	render(){
@@ -19,6 +25,7 @@ class ContactModal extends React.Component{
 				<div>
 				<h2>Please contact:{this.props.contactInfo} to get started!</h2>
 				</div>
+				<div onClick={this.closeModal}>This will be the button.</div>
 			</div>
 		)
 	}
---
+++ src/ContactUpdate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/******
+* Component that will be included when a user
+ * hasn't yet included their contact info
+ * for jobs
+ * *****/
